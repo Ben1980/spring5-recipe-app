ecfce299bcc11042ab02be0dcfff51589d203cf2
Benjamin Mahr <ben.mahr@gmail.com>
2018-07-22 20:59:09 +0200

    IndexController test finished

--- src/test/java/com/example/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/example/spring5recipeapp/controllers/IndexControllerTest.java
@@ -4,11 +4,13 @@ import com.example.spring5recipeapp.domain.Recipe;
 import com.example.spring5recipeapp.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anySet;
@@ -36,16 +38,27 @@ public class IndexControllerTest {
 
     @Test
     public void getIndexPage() {
-        Recipe recipe = new Recipe();
-        HashSet recipeSet = new HashSet();
-        recipeSet.add(recipe);
+        //given
+        Set<Recipe> recipeSet = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        Recipe recipe2 = new Recipe();
+        recipe1.setId(2L);
+        recipeSet.add(recipe1);
+        recipeSet.add(recipe2);
 
         when(recipeService.getRecipes()).thenReturn(recipeSet);
 
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String result = controller.getIndexPage(model);
 
+        //then
         assertEquals("index", result);
         verify(recipeService, times(2)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> inController = argumentCaptor.getValue();
+        assertEquals(2, inController.size());
     }
 }
